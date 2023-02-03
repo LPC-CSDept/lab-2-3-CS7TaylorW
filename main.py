20c418a227432263811cf15f8ac958091c91b6e5
Taylor W <twatanabe02021@gmail.com>
2023-02-03 02:16:44 -0800

    A2-3

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ def main():
     # Comlete your code here
     ##################################################
 
+    var1 = int(input("Please enter your first number."))
+    var2 = int(input("Please enter your second number."))
+    var3 = int(input("Please enter your third number."))
+    sum = (var1 + var2 + var3)
+    avg = (sum / 3)
+    print ('Values:', var1, var2, var3)
+    print ('Total:', sum)
+    print (f'Average: {avg:.2f}')
+
     pass
 
 
